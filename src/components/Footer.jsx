c71de81d4df2e4889280ea279229fe5284be76b2
Ryan Martin <ryan@meraki-digital.com>
2023-06-15 16:08:03 -0500

    cta and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,74 @@
-import { useTheme } from "../hooks/useTheme";
+import logoLight from "../../assets/shared/desktop/logo-light.png";
+import { Link } from "react-router-dom";
+import { BsFacebook } from "react-icons/bs";
+import { BsYoutube } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { BsPinterest } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
 
 export default function Footer() {
-	const { theme } = useTheme();
 	return (
-		<footer className="py-3" data-bs-theme={theme}>
-			<div className="text-center">
-				© 2021
-				<a className="ms-3" href="https://mdbootstrap.com/">
-					MDBootstrap.com
-				</a>
+		<footer className="bg-dark pb-5">
+			<div className="container">
+				<div className="footer-nav">
+					<div className="footer-nav-logo-wrapper">
+						<img className="logo" src={logoLight} alt="Designo" />
+					</div>
+					<ul>
+						<li>
+							<Link to="/company">OUR COMPANY</Link>
+						</li>
+						<li>
+							<Link to="/locations">LOCATIONS</Link>
+						</li>
+						<li>
+							<Link to="/contact">CONTACT</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="container">
+				<div className="footer-info">
+					<div className="footer-info-address">
+						<h3>Designo Central Office</h3>
+						<p className="mb-1">3886 Wellington St</p>
+						<p className="mb-0">Toronto, Ontario M9C 3J5</p>
+					</div>
+					<div className="footer-info-contact">
+						<h3>Contact Us (Central Office)</h3>
+						<p className="mb-1">P : +1 253-863-8967</p>
+						<p className="mb-0">M : contact@designo.co</p>
+					</div>
+					<div className="footer-info-social">
+						<ul>
+							<li>
+								<a href="https://www.facebook.com">
+									<BsFacebook />
+								</a>
+							</li>
+							<li>
+								<a href="https://www.youtube.com">
+									<BsYoutube />
+								</a>
+							</li>
+							<li>
+								<a href="https://www.twitter.com">
+									<BsTwitter />
+								</a>
+							</li>
+							<li>
+								<a href="https://www.pinterest.com">
+									<BsPinterest />
+								</a>
+							</li>
+							<li>
+								<a href="https://www.instagram.com">
+									<BsInstagram />
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</footer>
 	);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
 					</div>
 				</div>
 			</section>
-			<section className="container px-sm-0 section-spacer">
+			<section className="container px-sm-0 section-spacer-y">
 				<div className="row px-sm-0">
 					<div className="col-md-6">
 						<Link
@@ -47,26 +47,92 @@ export default function Home() {
 										</h3>
 									</div>
 								</Link>
-								<div className="col-12">
-									<Link
-										to="/projects"
-										className="card rounded-4 text-light gallery-card bg-3 gallery-card-split"
-									>
-										<div className="overlay h-100 d-flex flex-column justify-content-center align-items-center gap-4">
-											<h2>GRAPHIC DESIGN</h2>
-											<h3>
-												VIEW PROJECTS
-												<BsChevronRight className="text-primary ms-3" />
-											</h3>
-										</div>
-									</Link>
+							</div>
+							<div className="col-12">
+								<Link
+									to="/projects"
+									className="card rounded-4 text-light gallery-card bg-3 gallery-card-split"
+								>
+									<div className="overlay h-100 d-flex flex-column justify-content-center align-items-center gap-4">
+										<h2>GRAPHIC DESIGN</h2>
+										<h3>
+											VIEW PROJECTS
+											<BsChevronRight className="text-primary ms-3" />
+										</h3>
+									</div>
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="container features section-spacer-b">
+				<div className="row">
+					<div className="col-12 col-lg-4">
+						<div className="card border-0 flex-md-row flex-lg-column mb-5 mb-md-0">
+							<div className="card-img-top img-1"></div>
+							<div className="card-body text-center text-md-start text-lg-center">
+								<h3 className="my-4">PASSIONATE</h3>
+								<p>
+									Each project starts with an in-depth brand research to ensure
+									we only create products that serve a purpose. We merge art,
+									design, and technology into exciting new solutions.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="col-12 col-lg-4">
+						<div className="card border-0 flex-md-row flex-lg-column mb-5 mb-md-0">
+							<div className="card-img-top img-2"></div>
+							<div className="card-body text-center text-md-start text-lg-center">
+								<h3 className="my-4">RESOURCEFUL</h3>
+								<p>
+									Everything that we do has a strategic purpose. We use an agile
+									approach in all of our projects and value customer
+									collaboration. It guarantees superior results that fulfill our
+									clients’ needs.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="col-12 col-lg-4">
+						<div className="card border-0 flex-md-row flex-lg-column">
+							<div className="card-img-top img-3"></div>
+							<div className="card-body text-center text-md-start text-lg-center">
+								<h3 className="my-4">FREINDLY</h3>
+								<p>
+									We are a group of enthusiastic folks who know how to put
+									people first. Our success depends on our customers, and we
+									strive to give them the best experience a company can provide.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="container px-sm-0 text-light">
+				<div className="row px-sm-0">
+					<div className="col-12">
+						<div className="cta bg-primary rounded-4">
+							<div className="row">
+								<div className="col-12 col-lg-6 text-center text-lg-start">
+									<h2>Let's talk about your project</h2>
+									<p>
+										Ready to take it to the next level? Contact us today and
+										find out how our expertise can help your business grow.
+									</p>
+								</div>
+								<div className="col-12 col-lg-6 cta-btn-wrapper">
+									<button className="btn btn-light btn-cta">
+										GET IN TOUCH
+									</button>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</section>
-			<section className="py-4">
+			<section className="py-4 d-none">
 				<div className="container">
 					<div className="row">
 						<div className="col-6">
