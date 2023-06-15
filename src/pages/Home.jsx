fdda1e8d36e7946b98c93752cb895347c405b958
Ryan Martin <ryan@meraki-digital.com>
2023-06-14 22:19:19 -0500

    home banner tweaks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,21 @@
 import phoneImg from "../../assets/home/desktop/image-hero-phone.png";
 export default function Home() {
 	return (
-		<>
-			<section className="homeBanner bg-primary text-light">
-				<div className="container">
-					<div className="row">
-						<div className="col-md-6 text-center text-md-start">
-							<h1>
-								Award-winning custom designs and digital branding solutions
-							</h1>
-							<p className="mt-4">
-								With over 10 years in the industry, we are experienced in
-								creating fully responsive websites, app design, and engaging
-								brand experiences. Find out more about our services.
-							</p>
-							<button className="btn btn-light btn-cta mt-3">LEARN MORE</button>
-						</div>
+		<div className="home">
+			<section className="container homeBanner bg-primary text-light">
+				<div className="row">
+					<div className="col-md-6 text-center text-md-start">
+						<h1>Award-winning custom designs and digital branding solutions</h1>
+						<p className="mt-4">
+							With over 10 years in the industry, we are experienced in creating
+							fully responsive websites, app design, and engaging brand
+							experiences. Find out more about our services.
+						</p>
+						<button className="btn btn-light btn-cta mt-3">LEARN MORE</button>
 					</div>
 				</div>
 			</section>
+
 			<section className="py-4">
 				<div className="container">
 					<div className="row">
@@ -45,6 +42,6 @@ export default function Home() {
 					</div>
 				</div>
 			</section>
-		</>
+		</div>
 	);
 }
